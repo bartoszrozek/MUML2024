4842b40f20343002e6257b161dc86f4883d110c9
finwelf-euiv <finwelf@gmail.com>
2024-05-21 13:28:14 +0200

    rffs and tests of second dataset

--- src/rffs.py
+++ src/rffs.py
@@ -1,20 +1,26 @@
 from src.feature_selector_arbitrary import FeatureSelectorArbitrary
 import numpy as np
 import time
-from sklearn.linear_model import LassoCV
-from itertools import compress
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.feature_selection import SelectFromModel
 
 
-class LassoFS(FeatureSelectorArbitrary):
+class RandomForestFS(FeatureSelectorArbitrary):
     def run_fs(self):
         start = time.time()
         row_sums = self._data_frame_np.sum(axis=1)
         self._data_frame_np = self._data_frame_np / row_sums[:, np.newaxis]
         X = self._data_frame_np[:, 1:]
         y = self._data_frame_np[:, 0]
-        reg = LassoCV(cv=5, random_state=0).fit(X, y)
-        features = list(
-            compress(list(range(1, self._data_frame_np.shape[1]+1)), reg.coef_ != 0)
-        )
+        clf = ExtraTreesRegressor(n_estimators=50)
+        clf = clf.fit(X, y)
+        model = SelectFromModel(clf, prefit=True)
+        features = [i+1 for i in range(X.shape[1]) if model.get_support()[i-1]]
         self.time = time.time() - start
         self._features = features
+
+
+
+
+
+
